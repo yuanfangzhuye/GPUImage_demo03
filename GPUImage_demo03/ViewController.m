bbde82a748d9a79e6dbdafd35f57dde67b6cc75c
chao.li <chao.li@team-lab.cn>
2020-08-26 13:45:18 +0800

    初始化工程

--- GPUImage_demo03/ViewController.m
+++ GPUImage_demo03/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <GPUImage/GPUImage.h>
 
 @interface ViewController ()
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+target 'GPUImage_demo03' do
+  
+pod 'GPUImage'
+
+end
